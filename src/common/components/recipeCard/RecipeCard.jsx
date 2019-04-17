d4acaaa86cdbde993f209bab4dc3310bd03715d6
minnewanka <philippe.inthavong@gmail.com>
2019-04-16 23:19:28 -0400

    test

--- src/common/components/recipeCard/RecipeCard.jsx
+++ src/common/components/recipeCard/RecipeCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './RecipeCard.scss'
 
-const RecipeCard = ({ name, description }) => {
+const RecipeCard = ({ name, description, redirect }) => {
   return (
-    <div className='RecipeCard'>
-      <div className='card'>
-        <div className='Image'>
-          <div className='view-recipe'>VOIR LA RECETTE</div>
+    <div className="RecipeCard">
+      <div className="card">
+        <div className="Image" onClick={redirect}>
+          <div className="view-recipe">VOIR LA RECETTE</div>
         </div>
 
-        <div className='Text'>
+        <div className="Text">
           <h4>
             <b>{name}</b>
           </h4>
